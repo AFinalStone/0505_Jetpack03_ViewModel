592899fa3d756039b619969fd4ca211f25bc146d
shiyaolei <shiyaolei@yongrun.com>
2022-05-16 11:58:36 +0800

    项目初始化

---
+++ 0101_ViewModel/src/main/java/com/example/application/MApplication.java
@@ -0,0 +1,10 @@
+package com.example.application;
+
+import android.app.Application;
+
+public class MApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ 0101_ViewModel/src/main/java/com/example/router/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.router;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+// 在支持路由的页面上添加注解(必选)
+// 这里的路径需要注意的是至少需要有两级，/xx/xx
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
